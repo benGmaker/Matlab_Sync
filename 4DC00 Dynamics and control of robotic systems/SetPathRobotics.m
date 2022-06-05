0ebe74c726a677762ff1724c44ed845e907c949b
Ben Gortemaker <b.f.gortemaker@student.tue.nl>
2022-06-05 18:03:33 +0200

    gs 2

--- 4DC00 Dynamics and control of robotic systems/SetPathRobotics.m
+++ 4DC00 Dynamics and control of robotic systems/SetPathRobotics.m
@@ -9,13 +9,13 @@
 % other toolboxes.
 
 % Remove this line when you have set the variable folder below
-error('** 4DC00 - Change the path to match the correct folder on your laptop **')
+%error('** 4DC00 - Change the path to match the correct folder on your laptop **')
 
 % Add directories of the Robotics Toolbox to the path
 % [[ Linux/Mac users might need to use "/" in place of "\" ]]
 % Substitute "C:\" with the correct path on your machine
 
-folder = ‘SET HERE THE CORRECT FOLDER’;
+folder = pwd;
 
 addpath ( folder )
 addpath ( fullfile(folder, 'extrafunctions') ) % GSS 2 objplot
