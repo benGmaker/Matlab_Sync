253aaab0a1dee37899ddf30d32e9e7a21763ec42
Ben Gortemaker <b.f.gortemaker@student.tue.nl>
2024-06-14 21:55:07 +0200

    ik heb het denk ik gewoon gedaan

--- 4CM20 Hybrid Systems and Control/Exam 2/Question 2/CheckFeasibilityCvar.m
+++ 4CM20 Hybrid Systems and Control/Exam 2/Question 2/CheckFeasibilityCvar.m
@@ -1,25 +1,69 @@
-function [ub, lb, lbf, ubf] = CheckFeasibilityCvar(sigma,ub,lb,lbf,ubf,feasible)
-    if sigma == lb
-            lbf = feasible;    % lower bound feasible? 0: no, 1: yes
-        elseif sigma == ub
-            ubf = feasible;    % upper bound feasible? 0: no, 1: yes
+function [ub, lb, lbf, ubf,dwelltime_cond] = CheckFeasibilityCvar(sigma,ub,lb,lbf,ubf,dwelltime)
+    L = length(dwelltime); 
+    dwelltime_cond = 0;
+    if L == 1
+        %only one simulation has been done meaning we can't compare
+        %dwelltimes
+        lbf = 0;
+        return
+    end
     
-            % Resuls feasibility check if range is broadened
-        elseif sigma > ub
-            lb = ub;                % set new lower bound
-            ub = sigma;             % set new upper bound
-            ubf = feasible;    % determine feasibility upper bound
-        elseif sigma < lb
-            ub = lb;                % set new upper bound
-            lb = sigma;             % set new upper bound
-            lbf = feasible;    % determine feasibility lower bound
-    
-            % Determine feasibility after bisection
+    if ub < 0 
+        %cvar is negative, we are searching for maximal dwell time
+        dwelltime_cond = dwelltime(L) > dwelltime(L-1); 
+    else
+        %cvar is positive, we are searching for minimal dwell time
+        dwelltime_cond = dwelltime(L) < dwelltime(L-1); 
+    end
+
+
+    if L == 2 %second simulation run check
+        %two simulations have been done and we have dwelltime for the
+        %lowerboud (i = 1) and upper bound (i = 2) 
+        if dwelltime_cond
+            ubf = 0;
+            lbf = 1; % lower bound lower? 0: no, 1: yes
         else
-            if feasible
-                lb = sigma;     % new lower bound
-            else
-                ub = sigma;     % new upper bound
-            end
+            ubf = 1; % upper bound lower? 0: no, 1: yes
+            lbf = 0;
         end
+    elseif sigma == lb
+        if dwelltime_cond %feasible if dwelltime has decreased
+            lbf = 1; % lower bound feasible? 0: no, 1: yes
+        else
+            lbf = 0;
+        end
+    elseif sigma == ub
+        if dwelltime_cond %feasible if dwelltime has decreased
+            ubf = 1; % lower bound feasible? 0: no, 1: yes
+        else
+            ubf = 0;
+        end
+
+        % Resuls feasibility check if range is broadened
+    elseif sigma > ub
+        lb = ub;                % set new lower bound
+        ub = sigma;             % set new upper bound
+        if dwelltime_cond %feasible if dwelltime has decreased
+            lbf = 1; % lower bound feasible? 0: no, 1: yes
+        else
+            lbf = 0;
+        end
+    elseif sigma < lb
+        ub = lb;                % set new upper bound
+        lb = sigma;             % set new upper bound
+        if dwelltime_cond %feasible if dwelltime has decreased
+            ubf = 1; % lower bound feasible? 0: no, 1: yes
+        else
+            ubf = 0;
+        end
+
+        % Determine feasibility after bisection
+    else
+        if dwelltime_cond %feasible if dwelltime has decreased
+            lb = sigma;     % new lower bound
+        else
+            ub = sigma;     % new upper bound
+        end
+    end
 end
--- 4CM20 Hybrid Systems and Control/Exam 2/Question 2/LMI_q2f.m
+++ 4CM20 Hybrid Systems and Control/Exam 2/Question 2/LMI_q2f.m
@@ -1,10 +1,13 @@
-function [Pvar,Qvar,Zvar, L,diagnostics] = LMI_q2b(A,J,cvar,dvar,Pvar,Qvar,Zvar)
+function [Pvar,Qvar,Zvar, L,diagnostics] = LMI_q2f(A,J,cvar,dvar,Pvar,Qvar,Zvar)
 %UNTITLED10 Summary of this function goes here
 %   Detailed explanation goes here
         %setting LMI conditions
-        cond1 = A'* Pvar - Qvar' + Pvar*A - Qvar <= -cvar * Pvar;
-        cond2 = [ exp(-dvar)*Pvar,  J'*Pvar - Zvar';
-            Pvar*J - Zvar,     exp(dvar)*Pvar] >= 0;
+        L_1 = [6.7480;-3.0027;-0.0037];
+        L_2 = [0.5786;0.8427;0.4945];
+        C = [0, 1, 0];
+        cond1 = (A-L_1*C)'*Pvar + Pvar* (A - L_1*C) <= -cvar * Pvar;
+        cond2 = [ exp(-dvar)*Pvar, (J-L_2*C)'*Pvar ;
+            Pvar*(J-L_2*C),     exp(dvar)*Pvar] >= 0;
     
         %positive definite condition
         Lp = Pvar >= 1e-9;
